555dfaf50be30f8840cb8c1ef2bfdfe79d09dd05
Ajxrhaider <inimfonabasi125@gmail.com>
2026-07-14 14:28:24 +0100

    Please

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,6 +1,6 @@
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 import React, { createContext, useState, useEffect, useCallback } from 'react';
 import { jwtDecode } from 'jwt-decode';
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000';
 
 export const AppContext = createContext();
 
